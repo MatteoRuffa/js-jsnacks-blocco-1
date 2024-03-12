2da31c2cd60ba5a3b9e697a84270ca1f0f9add58
MatteoRuffa <cortese_17@protonmail.com>
2024-03-12 15:46:00 +0100

    add (bonus).js

--- array_bonus/(Bonus).js
+++ array_bonus/(Bonus).js
@@ -6,7 +6,11 @@ Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti 
 
 // Creiamo due array di numeri tra 1 e 100 che hanno un numero di elementi 
 // diversi scelti dall'utente
-let elements = getRndInteger(1, 100);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 
 const elUserArray1 = parseInt(prompt('Quanti numeri casuali da 1 a 100 vuoi prendere nel box 1?'));
 console.log(elUserArray1);
@@ -14,7 +18,7 @@ console.log(elUserArray1);
 let array1 = [];
 
 for(let i = 0; i < elUserArray1; i++){
-    elements  
+    let elements = getRndInteger(1, 100);
     array1.push(elements)
 };
 console.log(array1);
@@ -25,7 +29,7 @@ console.log(elUserArray2);
 let array2 = [];
 
 for(let i = 0; i < elUserArray2; i++){
-    elements = getRndInteger(1, 100);
+    let elements = getRndInteger(1, 100);
     array2.push(elements)
 };
 console.log(array2);
@@ -34,19 +38,23 @@ console.log(array2);
 // Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 //  troviamo la differemza di numeri di elementi
 // usiamo la differenza per creare in ciclo per pareggiare il numero di Elementi 
-if(array1 > array2) {
-    let differenzaArray = parseInt(array1.length - array2.length);
-    console.log(differenzaArray);
-    for (let i = 0; i < differenzaArray; i++) {
-        array2.push(elements[i])
-        console.log(array2);
+if(array1.length > array2.length) {
+    let differenceArray  = parseInt(array1.length - array2.length);
+    console.log(differenceArray );
+    for (let i = 0; i < differenceArray ; i++) {
+        let elements = getRndInteger(1, 100);
+        array2.push(elements)
     }
+    console.log(array2);
+    
 } else {
-    let differenzaArray = parseInt(array2.length - array1.length);
-    console.log(differenzaArray);
-    for (let i = 0; i < differenzaArray; i++) {
-        array1.push(elements[i])
+    let differenceArray  = parseInt(array2.length - array1.length);
+    console.log(differenceArray );
+    for (let i = 0; i < differenceArray ; i++) {
+        let elements = getRndInteger(1, 100);
+        array1.push(elements);
     }
+    console.log(array1);
 }
 
 
